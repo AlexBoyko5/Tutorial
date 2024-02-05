9b3bd2d8f33b6a1c7a6e69daab152baa847c3301
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-02-05 09:44:35 +0100

    Update Home work-9-2.js

--- Home work-9-2.js
+++ Home work-9-2.js
@@ -93,3 +93,51 @@ form.addEventListener('submit', (event) => {
 });
 
 loadStateFromLocalStorage();
+//&================= Task #2 исправл ==========================================
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+const form = document.querySelector('.feedback-form');
+
+
+const saveStateToLocalStorage = () => {
+    const state = {
+        email: emailInput.value.trim(),
+        message: messageInput.value.trim(),
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(state));
+};
+
+
+const loadStateFromLocalStorage = () => {
+    const savedState = localStorage.getItem('feedback-form-state');
+    if (savedState) {
+        const { email, message } = JSON.parse(savedState);
+        emailInput.value = email;
+        messageInput.value = message;
+    }
+};
+
+
+form.addEventListener('input', (event) => {
+    if (event.target === emailInput || event.target === messageInput) {
+        saveStateToLocalStorage();
+    }
+});
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    //~== что данные сохраняются в LocalStorage, вы можете изменить
+    //~==место вывода в консоль, чтобы отобразить сохраненные значения
+    //~== до их очистки. Т.е. console.log до localStorage.clear
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    localStorage.clear();
+    emailInput.value = '';
+    messageInput.value = '';
+    console.log({
+        email: emailInput.value,
+        message: messageInput.value,
+    });
+});
+
+loadStateFromLocalStorage();
